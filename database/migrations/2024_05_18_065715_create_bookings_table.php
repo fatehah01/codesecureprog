95cb561cbf57cc600b3f7d2d575fea034dc241a0
fatehah01 <83084450+fatehah01@users.noreply.github.com>
2024-05-18 20:20:31 +0800

    Add files via upload

---
+++ database/migrations/2024_05_18_065715_create_bookings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::create('bookings', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('car_id')->constrained()->onDelete('cascade');
+        $table->string('customer_name');
+        $table->string('customer_email');
+        $table->date('start_date');
+        $table->date('end_date');
+        $table->decimal('total_price', 8, 2);
+        $table->timestamps();
+    });
+}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
